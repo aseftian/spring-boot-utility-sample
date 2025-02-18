7f7feb683af0c1a4ced53b9ec1dac47e26a87690
Ardi Seftian <ardiseftian@gmail.com>
2025-02-18 13:34:15 +0700

    auto fit image to pdf page

--- src/main/java/id/ardisan/spring_boot_utility_sample/pdf/ItextPdfImage.java
+++ src/main/java/id/ardisan/spring_boot_utility_sample/pdf/ItextPdfImage.java
@@ -1,6 +1,11 @@
 package id.ardisan.spring_boot_utility_sample.pdf;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+
+import org.apache.commons.io.FileUtils;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Image;
@@ -8,33 +13,48 @@ import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.PdfWriter;
 
 public class ItextPdfImage {
-  
+
   public static void main(String... args) {
-    Document document = new Document();
-    document.addTitle("Itext PDF Document");
 
     try {
-      PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("Itextpdf.pdf"));
-      // Rectangle rect1 = new Rectangle(2480f, 3508f);
+      File img = new File("image1.jpg");
+      byte[] imgByte = FileUtils.readFileToByteArray(img);
+      Document document = new Document();
+      ByteArrayOutputStream outbyte = new ByteArrayOutputStream();
+      PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("PdfImage.pdf"));
       Rectangle rect1 = new Rectangle(595f, 842f); // ukukran A4 dalam pixel 72dpi
       document.setPageSize(rect1);
+      document.setMargins(55f, 55f, 55f, 55f);
       document.open();
+      Image image = Image.getInstance(imgByte);
+      float imgWidth = image.getWidth();
+      float imgHeight = image.getHeight();
 
-      Image image = Image.getInstance("image1.jpg");
-      // image.setAbsolutePosition(25f, 25f);
-      // image.setPaddingTop(25f);
-      // image.setTop(25f);
-      // image.setLeft(25f);
-      // System.out.println("Width: "+ document.getAccessibleAttribute(PdfName.WIDTH).toString());
-      // System.out.println("Height: "+ document.getAccessibleAttribute(PdfName.HEIGHT).toString());
-      // Rectangle rect1 = new Rectangle(210f, 297f);
-      image.scalePercent(50f);
+      float widthRatio = imgWidth / rect1.getWidth() * 100;
+      float heightRatio = imgHeight / rect1.getHeight() * 100;
+      System.out.println("widthRatio: " + widthRatio);
+      System.out.println("heightRatio: " + heightRatio);
+
+      if (widthRatio > 100f || heightRatio > 100f) {
+        if (widthRatio > heightRatio) {
+          float scaler = ((document.getPageSize().getWidth() - document.leftMargin()
+               - document.rightMargin() - 0) / image.getWidth()) * 100;
+          image.scalePercent(scaler);
+        } else {
+          float scaler = ((document.getPageSize().getHeight() - document.topMargin()
+               - document.bottomMargin() - 0) / image.getHeight()) * 100;
+          image.scalePercent(scaler);
+        }
+      }
+
       document.add(image);
 
       document.close();
       writer.close();
+
+      // return outbyte;
     } catch (Exception e) {
-      e.printStackTrace();
+      throw new RuntimeException(e.getMessage(), e);
     }
   }
 }
